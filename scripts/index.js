7b7bfa58cc0037d164f49fab64ecf029b3b53296
Elvira-dd <elvira.6.galieva@yandex.ru>
2022-12-26 20:32:44 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("mousemove", parallax);
+function parallax(event) {
+  this.querySelectorAll(".parallax-wrap span").forEach((shift) => {
+    const position = shift.getAttribute("value");
+    const x = (window.innerWidth - event.pageX * position) / 90;
+    const y = (window.innerHeight - event.pageY * position) / 90;
+
+    shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
+
+const element = document.querySelector("#second_s");
+
+element.addEventListener('wheel', (event) => {
+  event.preventDefault();
+
+  element.scrollBy({
+    left: event.deltaY < 0 ? -30 : 30,
+    
+  });
+});
